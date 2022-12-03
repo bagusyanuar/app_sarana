d204aa7aa854bb90bbd013e2c64f4955ac07c505
bagusyanuar <bagus.yanuar613@gmail.com>
2022-12-03 12:22:30 +0700

    components

---
+++ lib/components/base-card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class BaseCard extends StatelessWidget {
+  final VoidCallback? onTap;
+  const BaseCard({Key? key, this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/dummy/data.dart
@@ -0,0 +1,16 @@
+class DataDummy {
+  static List<Map<String, dynamic>> DummyProperties = [
+    {
+      "id": 1,
+      "name": "Personal Komputer",
+      "ruangan_id": 1,
+      "ruangan": {"id": 1, "nama": "Ruangan 1"}
+    },
+    {
+      "id": 1,
+      "name": "Personal Komputer",
+      "ruangan_id": 1,
+      "ruangan": {"id": 1, "nama": "Ruangan 1"}
+    }
+  ];
+}
