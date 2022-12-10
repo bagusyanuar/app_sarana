053f4313da96c8ef335488641f9cb4fa2a000956
bagusyanuar <bagus.yanuar613@gmail.com>
2022-12-10 13:17:03 +0700

    layout

--- lib/views/karyawan/sarana-out.dart
+++ lib/views/karyawan/sarana-out.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:app_sarana/dummy/data.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -13,7 +14,13 @@ class SaranaOut extends StatefulWidget {
 
 class _SaranaOutState extends State<SaranaOut> {
   String currentDate = '-- pilih tanggal --';
+  String? selectedRoom;
+  String? selectedSaranaRoom;
   List<Map<String, dynamic>> _listRoom = DataDummy.DummyRoom;
+  List<Map<String, dynamic>> _listSaranaRoom = DataDummy.DummySaranaRoom;
+  final TextEditingController _textRoomController = TextEditingController();
+  final TextEditingController _textSaranaRoomController =
+      TextEditingController();
 
   @override
   void initState() {
@@ -23,6 +30,14 @@ class _SaranaOutState extends State<SaranaOut> {
     setState(() {
       currentDate = DateFormat('yyyy-MM-dd').format(now);
     });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _textRoomController.dispose();
+    _textSaranaRoomController.dispose();
+    super.dispose();
   }
 
   @override
@@ -107,11 +122,168 @@ class _SaranaOutState extends State<SaranaOut> {
                     ),
                     Container(
                       margin: const EdgeInsets.only(bottom: 5),
+                      padding: const EdgeInsets.only(left: 10, right: 5),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton2(
+                          isExpanded: true,
+                          hint: Text(
+                            "Pilih Ruangan",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Theme.of(context).hintColor,
+                            ),
+                          ),
+                          items: _listRoom
+                              .map(
+                                (e) => DropdownMenuItem(
+                                  value: e["id"].toString(),
+                                  child: Text(e["name"]),
+                                ),
+                              )
+                              .toList(),
+                          value: selectedRoom,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedRoom = value as String;
+                            });
+                          },
+                          buttonHeight: 50,
+                          buttonWidth: MediaQuery.of(context).size.width,
+                          itemHeight: 40,
+                          dropdownMaxHeight: 250,
+                          searchController: _textRoomController,
+                          searchInnerWidget: Padding(
+                            padding: const EdgeInsets.only(
+                              top: 8,
+                              bottom: 4,
+                              right: 8,
+                              left: 8,
+                            ),
+                            child: TextFormField(
+                              controller: _textRoomController,
+                              decoration: InputDecoration(
+                                isDense: true,
+                                contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                  vertical: 8,
+                                ),
+                                hintText: 'Search for an item...',
+                                hintStyle: const TextStyle(fontSize: 12),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                          ),
+                          searchMatchFn: (item, searchValue) {
+                            return (item.value
+                                .toString()
+                                .contains(searchValue));
+                          },
+                          onMenuStateChange: (isOpen) {
+                            if (!isOpen) {
+                              _textRoomController.clear();
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 5),
                       child: const Text("Sarana / Prasarana"),
                     ),
                     Container(
                       margin: const EdgeInsets.only(bottom: 5),
-                      child: const Text("Keterangan"),
+                      padding: const EdgeInsets.only(left: 10, right: 5),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton2(
+                          isExpanded: true,
+                          hint: Text(
+                            "Pilih Sarana",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Theme.of(context).hintColor,
+                            ),
+                          ),
+                          items: _listSaranaRoom
+                              .map(
+                                (e) => DropdownMenuItem(
+                                  value: e["id"].toString(),
+                                  child: Text(e["name"]),
+                                ),
+                              )
+                              .toList(),
+                          value: selectedSaranaRoom,
+                          onChanged: (value) {
+                            log(value as String);
+                            setState(() {
+                              selectedSaranaRoom = value as String;
+                            });
+                          },
+                          buttonHeight: 50,
+                          buttonWidth: MediaQuery.of(context).size.width,
+                          itemHeight: 40,
+                          dropdownMaxHeight: 250,
+                          searchController: _textSaranaRoomController,
+                          searchInnerWidget: Padding(
+                            padding: const EdgeInsets.only(
+                              top: 8,
+                              bottom: 4,
+                              right: 8,
+                              left: 8,
+                            ),
+                            child: TextFormField(
+                              controller: _textSaranaRoomController,
+                              decoration: InputDecoration(
+                                isDense: true,
+                                contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                  vertical: 8,
+                                ),
+                                hintText: 'Search for an item...',
+                                hintStyle: const TextStyle(fontSize: 12),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                          ),
+                          searchMatchFn: (item, searchValue) {
+                            log(item.value);
+                            return (item.value
+                                .toString()
+                                .contains(searchValue));
+                          },
+                          onMenuStateChange: (isOpen) {
+                            if (!isOpen) {
+                              _textSaranaRoomController.clear();
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 5),
+                      child: const Text("Keterangan / Keperluan"),
+                    ),
+                    TextField(
+                      onChanged: (text) {},
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          hintText: "Keterangan / Keperluan"),
+                      maxLines: 5,
                     ),
                   ],
                 ),
@@ -122,6 +294,24 @@ class _SaranaOutState extends State<SaranaOut> {
                 decoration: BoxDecoration(
                   color: Colors.blue,
                   borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.save,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "SIMPAN",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
               )
             ],
